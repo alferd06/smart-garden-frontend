4a344c24c35bc36dccc0053ca5b644a135646ccc
alferd06 <alwiferdian6.5@gmail.com>
2026-05-09 20:55:20 +0700

    useEffect test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from "react"
+
 function App() {
+  const [suhu, setSuhu] = useState(28)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const suhuBaru = +(22 + Math.random() * 10).toFixed(1)
+      setSuhu(suhuBaru)
+    }, 2000) // setiap 2 detik
+
+    return () => clearInterval(interval) // cleanup
+  }, []) // [] = jalankan sekali saja waktu pertama muncul
+
   return (
     <div>
-      <h1>Dashboard Kebun Selaawi</h1>
-      <p>Pemantauan sensor real-time</p>
+      <p>Suhu Kebun</p>
+      <h1>{suhu}°C</h1>
     </div>
   )
 }
